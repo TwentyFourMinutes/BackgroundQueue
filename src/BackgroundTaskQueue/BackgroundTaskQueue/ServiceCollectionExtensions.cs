ea5104b2eee186c6326747fe27c3375213475da7
Twenty <twenty@translucent.at>
2019-11-01 20:46:26 +0100

    Added ServiceCollection extensions

---
+++ src/BackgroundTaskQueue/BackgroundTaskQueue/ServiceCollectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace BackgroundTaskQueue.Core
+{
+	public static class ServiceCollectionExtensions
+	{
+		public static IServiceCollection AddBackgroundTaskQueue(this IServiceCollection services)
+			=> services.AddSingleton<IBackgroundTaskQueue, BackgroundTaskQueue>()
+					   .AddHostedService<BackgroundQueueService>();
+	}
+}
